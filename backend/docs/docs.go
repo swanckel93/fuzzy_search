08849278010f0e2c36a94459b9f1af26888e73f0
Stefan Wanckel <stefanwanckel@gmail.com>
2025-04-16 17:19:05 +0200

    recreated swagger json

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -220,12 +220,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
+	Version:          "1.0",
 	Host:             "",
-	BasePath:         "",
+	BasePath:         "/",
 	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
+	Title:            "Fuzzy Search API",
+	Description:      "A simple fuzzy text search API with file upload, search, and caching.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
